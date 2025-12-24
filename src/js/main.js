025b9e7b66f6508609bfd9020efcfd848680df31
977ADAM <977team.95@gmail.com>
2025-12-24 21:29:44 +0300

    add main

--- src/js/main.js
+++ src/js/main.js
@@ -16,33 +16,33 @@
 
 
 
-const TOTAL_TIME = 5 * 60; // 5 минут
-let timeLeft = TOTAL_TIME;
+// const TOTAL_TIME = 5 * 60; // 5 минут
+// let timeLeft = TOTAL_TIME;
 
-const timeEl = document.getElementById("time");
-const progressCircle = document.querySelector(".ring-progress");
+// const timeEl = document.getElementById("time");
+// const progressCircle = document.querySelector(".ring-progress");
 
-const radius = 70;
-const circumference = 2 * Math.PI * radius;
+// const radius = 70;
+// const circumference = 2 * Math.PI * radius;
 
-progressCircle.style.strokeDasharray = circumference;
-progressCircle.style.strokeDashoffset = 0;
+// progressCircle.style.strokeDasharray = circumference;
+// progressCircle.style.strokeDashoffset = 0;
 
-function updateTimer() {
-  const minutes = Math.floor(timeLeft / 60);
-  const seconds = timeLeft % 60;
+// function updateTimer() {
+//   const minutes = Math.floor(timeLeft / 60);
+//   const seconds = timeLeft % 60;
 
-  timeEl.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+//   timeEl.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 
-  const progress = timeLeft / TOTAL_TIME;
-  progressCircle.style.strokeDashoffset = circumference * (1 - progress);
+//   const progress = timeLeft / TOTAL_TIME;
+//   progressCircle.style.strokeDashoffset = circumference * (1 - progress);
 
-  if (timeLeft > 0) {
-    timeLeft--;
-  } else {
-    clearInterval(timer);
-  }
-}
+//   if (timeLeft > 0) {
+//     timeLeft--;
+//   } else {
+//     clearInterval(timer);
+//   }
+// }
 
-updateTimer();
-const timer = setInterval(updateTimer, 1000);
+// updateTimer();
+// const timer = setInterval(updateTimer, 1000);
