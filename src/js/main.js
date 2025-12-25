0a6086b174dcc908002b30a67ec62f7f592b6d81
977ADAM <977team.95@gmail.com>
2025-12-25 23:50:03 +0300

    finish promo

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,30 @@
-// let time = 300; // 5 минут
+const TOTAL_TIME = 5 * 60; // 5 минут
+let timeLeft = TOTAL_TIME;
 
-// const timerEl = document.querySelector('.timer-text');
+const timeEl = document.getElementById("time");
+const progressCircle = document.querySelector(".ring-progress");
 
-// const tick = () => {
-//   const minutes = String(Math.floor(time / 60)).padStart(2, '0');
-//   const seconds = String(time % 60).padStart(2, '0');
+const radius = 70;
+const circumference = 2 * Math.PI * radius;
 
-//   timerEl.textContent = `${minutes}:${seconds}`;
+progressCircle.style.strokeDasharray = circumference;
+progressCircle.style.strokeDashoffset = 0;
 
-//   if (time > 0) time--;
-// };
+function updateTimer() {
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
 
-// tick();
-// setInterval(tick, 1000);
+  timeEl.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 
+  const progress = timeLeft / TOTAL_TIME;
+  progressCircle.style.strokeDashoffset = circumference * (1 - progress);
 
+  if (timeLeft > 0) {
+    timeLeft--;
+  } else {
+    clearInterval(timer);
+  }
+}
 
-// const TOTAL_TIME = 5 * 60; // 5 минут
-// let timeLeft = TOTAL_TIME;
-
-// const timeEl = document.getElementById("time");
-// const progressCircle = document.querySelector(".ring-progress");
-
-// const radius = 70;
-// const circumference = 2 * Math.PI * radius;
-
-// progressCircle.style.strokeDasharray = circumference;
-// progressCircle.style.strokeDashoffset = 0;
-
-// function updateTimer() {
-//   const minutes = Math.floor(timeLeft / 60);
-//   const seconds = timeLeft % 60;
-
-//   timeEl.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
-
-//   const progress = timeLeft / TOTAL_TIME;
-//   progressCircle.style.strokeDashoffset = circumference * (1 - progress);
-
-//   if (timeLeft > 0) {
-//     timeLeft--;
-//   } else {
-//     clearInterval(timer);
-//   }
-// }
-
-// updateTimer();
-// const timer = setInterval(updateTimer, 1000);
+updateTimer();
+const timer = setInterval(updateTimer, 1000);
